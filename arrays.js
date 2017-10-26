4a1eb711facebc97eb1d485d9039874c35ec6aeb
Robert Meis <robert.meis@colorado.edu>
2017-10-26 22:07:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -55,3 +55,9 @@ function removeElementFromEndOfArray(array) {
 
   return array.slice(0, array.length-1);
 }
+
+
+function removeElementFromBeginningOfArray(array) {
+
+  return array.slice(1, array.length);
+}
