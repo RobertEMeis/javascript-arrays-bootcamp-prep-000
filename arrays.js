4cb437626348fbe796cbd6df4a28232b49375dce
Robert Meis <robert.meis@colorado.edu>
2017-10-26 19:55:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,8 +4,5 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element) {
 
   return array = [element, ...array];
-  
+
 }
-
-
-
