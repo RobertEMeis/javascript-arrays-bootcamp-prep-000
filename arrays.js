348d5d9053c3812751908885dcbbfffd9bd7079b
Robert Meis <robert.meis@colorado.edu>
2017-10-26 20:01:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function addElementToBeginningOfArray(array, element) {
 function destructivelyAddElementToBeginningOfArray(array, element) {
 
 
-  array[1] = element;
+  array[0] = element;
   return array;
 
 }
